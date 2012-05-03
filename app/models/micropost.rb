50e63ae5f4eb9c6d1e0239fefdc5a3086d55cbc8
mukul saharia <mukul85in@gmail.com>
2012-05-03 13:49:07 +0530

    micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
-   validates :content, :length => { :maximum => 20 }
+  belongs_to :user
+     validates :content, :length => { :maximum => 20 }
 end
